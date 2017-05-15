673ea13b630c3d47f5d7ae58844419c7c774e569
Kumicak Stanislav <stanislav.kumicak@tieto.com>
2017-05-15 06:40:07 +0200

    56.ref client package is to be used

--- run_client.bat
+++ run_client.bat
@@ -1,4 +1,4 @@
 del *.class
 javac -cp ..;..\lib\swing-layout-1.0.jar Main.java
-start javaw -cp ..;..\lib\swing-layout-1.0.jar chat.Main
+start javaw -cp ..;..\lib\swing-layout-1.0.jar chat.Main sk1u04g4
 
