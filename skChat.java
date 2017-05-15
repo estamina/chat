3a443c2894b6fcba7e31c2f19e868be3e26edd80
Kumicak Stanislav <stanislav.kumicak@tieto.com>
2017-05-15 08:37:23 +0200

    60

---
+++ skChat.java
@@ -0,0 +1,25 @@
+/*
+ * skChat.java
+ *
+ * Created on Pondelok, 2007, jún 25, 8:14
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+package chat;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author sk1u04g4
+ */
+    public final class skChat {
+        public String name=null;
+        
+        /**
+         * participats on this skChat
+         */
+        public LinkedList users=new LinkedList();
+
+        public int hash;
+    }
